5167aaf0484314be6ec2edcd33125aa864d3d470
Aleksandr Pavlov <ckidoz@gmail.com>
2026-07-12 19:27:31 +0300

    Update vulners-cli to v1.4.0

--- Formula/vulners-cli.rb
+++ Formula/vulners-cli.rb
@@ -1,16 +1,16 @@
 class VulnersCli < Formula
   desc "CLI vulnerability scanner powered by Vulners"
   homepage "https://github.com/kidoz/vulners-cli"
-  version "1.3.0"
+  version "1.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kidoz/vulners-cli/releases/download/v1.3.0/vulners-cli_1.3.0_darwin_arm64.tar.gz"
-      sha256 "5082d40a03986cc54b1ac6fb19534312e7e039284e3ef68cd514db05fa96789f"
+      url "https://github.com/kidoz/vulners-cli/releases/download/v1.4.0/vulners-cli_1.4.0_darwin_arm64.tar.gz"
+      sha256 "50258d58d7418305e051c4f5e7a9522d2ef7a46340ceb7a6d6241c1d929084e3"
     else
-      url "https://github.com/kidoz/vulners-cli/releases/download/v1.3.0/vulners-cli_1.3.0_darwin_amd64.tar.gz"
-      sha256 "d29c3102a07f90e376d4e2edd89b6263de6545c602b6df5108284f55928fb2df"
+      url "https://github.com/kidoz/vulners-cli/releases/download/v1.4.0/vulners-cli_1.4.0_darwin_amd64.tar.gz"
+      sha256 "99bfbffa923a61e4833f2485aa49c23c57823a113790f9521b71fdec13412be7"
     end
   end
 
